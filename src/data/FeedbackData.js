9e84eb825e94c6089b39743d099b66756db2bfad
Vijayakumar-Harish <vijayakumarharisha@gmail.com>
2025-09-26 00:30:13 +0530

    feedback app

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData = [
+    {
+      "id": 1,
+      "rating": 9,
+      "text": "This is feedback item 1 coming from the backend"
+    },
+    {
+      "id": 2,
+      "rating": 8,
+      "text": "This is feedback item 2 coming from the backend"
+    },
+    {
+      "text": "Updated all packages to latest",
+      "rating": 3,
+      "id": 3
+    }
+]
+
+
+export default FeedbackData;
